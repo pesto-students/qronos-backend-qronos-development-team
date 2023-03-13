bd7bdf0679de585c5dfad555f3957a0705442952
unknown <30varanshu@gmail.com>
2023-03-13 20:41:07 +0530

    complete get user api

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,41 +4,21 @@ const User = require('../models/user.models')
 const getUser = async (req, res) => {
     const value = await User.findOne({ email: '30varanshu@gmail.com' })
     console.log("allValues", value);
+
+    let newCreatedValue;
+
     if (value) {
         // res.status(200).send(JSON.stringify(value))
         // if (value.database.length < 0) {
 
         // }
-        const database = {
-            name: 'Hello'
-        }
-        // await User.findOneAndUpdate(
-        //     { email: '30varanshu@gmail.com' },
-        //     { $push: { database: database } },
-        //     { new: true }
-        // )
-
-        const product = {
-            title: 'Product 2',
-            description: 'This is Product 2',
-            thumbnailTitle: 'Product 2',
-            seoTitle: 'Product 2',
-            seoDescription: 'Product 2',
-        }
-
-        await User.updateOne(
-            {
-                email: '30varanshu@gmail.com',
-                'database._id': '640df52fc54a34c00c2b614d'
-            },
-            {
-                $push: { 'database.$.productTable': product }
-            }
-        )
+        // const database = {
+        //     name: 'Hello'
+        // }
     } else {
-        await createUser(req, res)
+        newCreatedValue = await createUser(req, res)
     }
-    res.status(200).send(JSON.stringify(value))
+    res.status(200).send(JSON.stringify(value ? value : newCreatedValue))
 }
 
 const createUser = async (req, res) => {
@@ -50,8 +30,34 @@ const createUser = async (req, res) => {
         // database: []
     })
     console.log(userValues);
+    return userValues
 }
 
 module.exports = {
     getUser
 }
+
+
+        // await User.findOneAndUpdate(
+        //     { email: '30varanshu@gmail.com' },
+        //     { $push: { database: database } },
+        //     { new: true }
+        // )
+
+        // const product = {
+        //     title: 'Product 2',
+        //     description: 'This is Product 2',
+        //     thumbnailTitle: 'Product 2',
+        //     seoTitle: 'Product 2',
+        //     seoDescription: 'Product 2',
+        // }
+
+        // await User.updateOne(
+        //     {
+        //         email: '30varanshu@gmail.com',
+        //         'database._id': '640df52fc54a34c00c2b614d'
+        //     },
+        //     {
+        //         $push: { 'database.$.productTable': product }
+        //     }
+        // )
