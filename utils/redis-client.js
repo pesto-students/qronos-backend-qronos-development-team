fa72157b993a10fbf6d4f82a9e344eac3f4079b8
unknown <30varanshu@gmail.com>
2023-04-13 00:31:41 +0530

    minor changes to redis connection part 3

--- utils/redis-client.js
+++ utils/redis-client.js
@@ -1,21 +1,20 @@
 const redis = require('redis');
 // (async () => {
-const client = process.env.NODE_ENV === "production"
-    ? redis.createClient({
-        // socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-        // },
-        username: process.env.REDIS_USERNAME,
-        password: process.env.REDIS_PWD
+let client
+
+if (process.env.NODE_ENV === "production")
+    client = redis.createClient({
+        url: 'rediss://red-cgrfa8rk9u56e3mh0te0:MpSGU3inJxvUMFPJ8bjHooBnCK98fH4C@singapore-redis.render.com:6379'
     })
-    : redis.createClient();
+else
+    client = redis.createClient();
 
 client.on('error', (error) => {
     console.log(process.env.NODE_ENV === "production", !!process.env.PRODUCTION);
     console.log(process.env.REDIS_HOST, process.env.REDIS_PORT, process.env.REDIS_USERNAME, process.env.REDIS_PWD);
     console.log(`redis error ${error}`)
 })
+console.log("client", client)
 client.connect()
 // })
 
